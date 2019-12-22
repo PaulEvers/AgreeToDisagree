125d93940ea82070bb253bb398a1be0dd156e6a6
Paul <paulusman@live.nl>
2019-12-22 15:11:59 +0100

    Firebase

--- AgreeToDisagree/src/environments/environment.ts
+++ AgreeToDisagree/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAuSIOwdt-3mYAUoixTo4DVW84Qt4bK4ZM',
+    authDomain: 'agreetodisagree-1c182.firebaseapp.com',
+    databaseURL: 'https://agreetodisagree-1c182.firebaseio.com',
+    projectId: 'agreetodisagree-1c182',
+    storageBucket: 'agreetodisagree-1c182.appspot.com',
+    messagingSenderId: '149526758346',
+    appId: '1:149526758346:web:00714f0f51088f0daa4533',
+    measurementId: 'G-YTLP84CLYH'
+  }
 };
 
 /*
