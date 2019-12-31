27013670960d7d82842cb53b4d8197f764dec635
Paul <paulusman@live.nl>
2019-12-31 15:40:04 +0100

    Styled component

--- AgreeToDisagree/src/app/input-question-view/input-question-view.module.ts
+++ AgreeToDisagree/src/app/input-question-view/input-question-view.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputQuestionComponent } from './input-question/input-question.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 
 @NgModule({
   declarations: [InputQuestionComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   exports: [InputQuestionComponent]
 })
